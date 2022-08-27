538ec41ac6b081bc555ced4906193d9d07bac715
taichi-danno-git <taichi.danno.git@gmail.com>
2022-08-27 13:15:01 +0900

    ユーザインターフェース作成　登録画面(activity_regist.xml)、項目名を囲っているFrameLayoutを削除して検証する前の段階

---
+++ app/src/main/java/com/example/employee_managementsystem/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.employee_managementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regist)
+    }
+}
