6dfdb86e24fdceb76a2647f53191eedc7f316f15
taichi-danno-git <taichi.danno.git@gmail.com>
2022-08-30 17:06:17 +0900

    UI実装　実装レビュー承認

--- app/src/main/java/com/example/employee_managementsystem/MainActivity.kt
+++ app/src/main/java/com/example/employee_managementsystem/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.employee_managementsystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var register_button: Button = findViewById(R.id.register_button)
+
+        register_button.setOnClickListener{
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/employee_managementsystem/RegisterActivity.kt
+++ app/src/main/java/com/example/employee_managementsystem/RegisterActivity.kt
@@ -2,10 +2,17 @@ package com.example.employee_managementsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regist)
+
+        val main_button : Button = findViewById(R.id.main_button)
+
+        main_button.setOnClickListener{
+            finish()
+        }
     }
 }
